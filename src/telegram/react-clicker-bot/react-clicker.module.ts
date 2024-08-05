e0a990c7cd2c725c110dae19e4744be9a72f31ee
phillbot <d.philippenko@gmail.com>
2024-08-05 15:52:17 +0300

    Improvement: Add session limit for clicker bot

--- src/telegram/react-clicker-bot/react-clicker.module.ts
+++ src/telegram/react-clicker-bot/react-clicker.module.ts
@@ -5,10 +5,16 @@ import { ReactClickerBotPlayCommand, ReactClickerBotStartCommand } from '@telegr
 
 import { ReactClickerBot } from './react-clicker.bot';
 import { defaultLang, supportedLangs } from './react-clicker.utils';
-import { ReactClickerBotToken, ReactClickerDefaultLang, ReactClickerSupportedLangs } from './symbols';
+import {
+  ReactClickerBotToken,
+  ReactClickerDefaultLang,
+  ReactClickerSessionDuration,
+  ReactClickerSupportedLangs,
+} from './symbols';
 
 export const reactClickerBotModule = new ContainerModule((bind: interfaces.Bind) => {
   bind<string>(ReactClickerBotToken.$).toConstantValue(process.env.REACT_CLICKER_BOT_TOKEN!);
+  bind<string>(ReactClickerSessionDuration.$).toConstantValue(process.env.REACT_CLICKER_APP_SESSION_DURATION_S!);
   bind<LanguageCode[]>(ReactClickerSupportedLangs.$).toConstantValue(supportedLangs);
   bind<LanguageCode>(ReactClickerDefaultLang.$).toConstantValue(defaultLang);
 
